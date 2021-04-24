61911ed126485fac1c3d76056d02a4b77e5e5b01
KrishnanS2006 <62958782+KrishnanS2006@users.noreply.github.com>
2021-04-24 18:49:37 -0400

    Fixed some stuff 🤷‍

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const port = 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/friends', (req, res) => {
+  // require('./getFriends')(req, res);
   const genres = require('./spotifyAPI')(req, res);
+  const percentGenres = require('./getDataAsPercent')(genres);
+})
 
-  const precentGenre = require('./getDataAsPercent')(genres)
-
-  
-  
 app.get('/spotify-auth', (req, res) => {
   require('./spotifyAPI')(req, res);
+  require('./getFriends')(req, res);
 })
 
 
--- spotifyAPI.js
+++ spotifyAPI.js
@@ -1,5 +1,9 @@
+const fetch = require('node-fetch');
+const { URLSearchParams } = require('url');
+
 module.exports = (req, res) =>{
     code = req.body.code;
     state = req.body.state;
-
+    const params = new URLSearchParams();
+    "https://accounts.spotify.com/api/token"
 }
