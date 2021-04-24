4a9036caae53982b4e8c7fd4cde48b556b8039d9
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2021-04-24 13:06:28 -0700

    Adding file for organization

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  
+
+  require('./spotifyAPI')(req, res);
+  
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
