11737611719ca0852f1b0f903d916800b25ae620
KrishnanS2006 <62958782+KrishnanS2006@users.noreply.github.com>
2021-04-24 22:31:17 -0400

    Spotify API data part works

--- src/spotifyAPI.js
+++ src/spotifyAPI.js
@@ -3,43 +3,44 @@ const { URLSearchParams } = require('url');
 
 
 module.exports = async (req, res) =>{
+    // Get Token from provided Access Code
     code = req.body.code;
-    // state = req.body.state;
-
-
-    // let paramaters = {
-    //     grant_type: 'authorization_code',
-    //     code: code,
-    //     redirect_uri: 'http://localhost:8080',
-    //     client_id: 'dcf1a2dc85864c11aaf00c98daed0e8a',
-    //     client_secret: '981eb984ccc842dd9b349b7273738cbc'
-    // };
-
-    const params = new URLSearchParams();
-
+    let params = new URLSearchParams();
     params.append('grant_type', 'authorization_code');
     params.append('code', code);
     params.append('redirect_uri', 'http://localhost:8080');
     params.append('client_id', 'dcf1a2dc85864c11aaf00c98daed0e8a');
     params.append('client_secret', '981eb984ccc842dd9b349b7273738cbc');
-
-
-    // console.log(params)
-
-
-    const response = await fetch('https://accounts.spotify.com/api/token', {
+    let response = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         body: params,
         headers: { 'Content-Type': 'application/x-www-form-urlencoded'}
     })
+    let data = await response.json();
+    const token = data["access_token"]
+    const tokenB = "Bearer " + token
 
-    const data = await response.json();
+    // Get Recently Played Songs from Token
+    response = await fetch('https://api.spotify.com/v1/me/player/recently-played?limit=50', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': tokenB }
+    })
+    data = await response.json();
+    // USE DATA ABOVE TO CALCULATE NUMBERS FOR GENRE.
+    // Data Format: https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-recently-played
 
-    console.log(data)
-    
-    
-    // .then(res => res.json())
-    //   .then(json => console.log(json));
-    
+    // Get User Info from Token
+    if (true) {
+        // CHANGE 'TRUE' TO A CONDITION THAT CHECKS IF USER IS ALREADY IN SQL DATABASE
+        response = await fetch('https://api.spotify.com/v1/me', {
+            method: 'GET',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': tokenB }
+        })
+        data = await response.json();
+        let display_name = data["display_name"];
+        let email = data["email"];
+        let username = data["id"];
+        // USE VARIABLES ABOVE TO ADD A NEW ROW IN SQL DATABASE
+    }
 
 }
